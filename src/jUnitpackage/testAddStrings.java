f6295c17724224eae3575baa66112d6af4350a44
sreeram.saipuneeth <sreeram.saipuneeth@CSELAB02-24.bennett.edu.in>
2017-09-26 11:43:48 +0530

    jUnit testing

---
+++ src/jUnitpackage/testAddStrings.java
@@ -0,0 +1,17 @@
+package jUnitpackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test() {
+		jUnitFunction junitstring = new jUnitFunction();
+		String result = junitstring.addStrings("capstone", "project");
+		assertEquals("capstoneproject", result);
+		
+	}
+
+}
